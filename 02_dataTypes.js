0db922578dd281e667b8c491ba72e9f330b00451
Tauqueer019 <tauqueer987@gmail.com>
2024-06-23 16:35:03 +0530

    con

--- 02_dataTypes.js
+++ 02_dataTypes.js
@@ -6,3 +6,13 @@
 let name = "tauqeer"
 let age = 18
 let isLoggedIn = false
+
+
+// number => 2 to power 53
+//bigInt
+// string => ""
+//boolean => true/false
+//null => standalone value
+// undefined =>
+// symbol =>
+// object        
